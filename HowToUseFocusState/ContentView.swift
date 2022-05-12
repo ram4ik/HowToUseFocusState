468f476643df972c3e49915a1b5a9df9f93247cb
Ramill <ramill_ibragimov@icloud.com>
2022-05-12 09:00:29 +0300

    init

--- HowToUseFocusState/ContentView.swift
+++ HowToUseFocusState/ContentView.swift
@@ -7,10 +7,43 @@
 
 import SwiftUI
 
+enum FocusText {
+    case username, password
+}
+
 struct ContentView: View {
+    @FocusState private var focusField: FocusText?
+    @State private var usernameText = ""
+    @State private var passwordText = ""
+    @State private var submittedText = "Submit credentials"
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Form {
+            Section("Credentials") {
+                TextField("Enter a username", text: $usernameText)
+                    .focused($focusField, equals: .username)
+                    .submitLabel(.next)
+                    .onSubmit {
+                        focusField = .password
+                    }
+                
+                TextField("Enter a password", text: $passwordText)
+                    .focused($focusField, equals: .password)
+                    .submitLabel(.next)
+                    .onSubmit {
+                        submitData()
+                    }
+            }
+            Section("Submitted") {
+                Text(submittedText)
+            }
+        }
+        
+    }
+    
+    private func submitData() {
+        focusField = nil
+        submittedText = "\(usernameText), \(passwordText)"
     }
 }
 
