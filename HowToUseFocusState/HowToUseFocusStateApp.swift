03d57e1c49a82eacb1006c177e241ba7c16e65c5
Ramill <ramill_ibragimov@icloud.com>
2022-05-12 08:49:22 +0300

    Initial Commit

---
+++ HowToUseFocusState/HowToUseFocusStateApp.swift
@@ -0,0 +1,17 @@
+//
+//  HowToUseFocusStateApp.swift
+//  HowToUseFocusState
+//
+//  Created by Ramill Ibragimov on 12.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct HowToUseFocusStateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
